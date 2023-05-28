6fd47483d82eacbae6755c8b1fce5c70cdbeaba6
Jonathan Dorety <jonathan.dorety@gmail.com>
2023-05-28 16:52:17 -0400

    Made the seedling of a page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	const copyright = new Date().getFullYear();
+</script>
+
+<div class="page-container">
+	<h1>Jonathan Dorety</h1>
+	<slot />
+	<footer>
+		<p>Copyright {copyright} Jonathan Dorety</p>
+	</footer>
+</div>
+
+<style>
+	h1 {
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',
+			Geneva, Verdana, sans-serif;
+		font-size: 2.75rem;
+		text-align: center;
+	}
+	div.page-container {
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Full-Stack Web Developer</h2>
+<p>Hire me for your next gig!</p>
