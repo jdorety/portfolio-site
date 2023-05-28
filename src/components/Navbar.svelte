9e63bd9850491e0900c469c92c3cce722634bc27
Jonathan Dorety <jonathan.dorety@gmail.com>
2023-05-28 17:16:27 -0400

    Dummy out a nav bar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,24 @@
+<nav>
+	<div class="link-bar" role="navigation">
+		<a href="/">Home</a>
+		<a href="#">About</a>
+		<a href="#">Contact</a>
+	</div>
+</nav>
+
+<style>
+	.link-bar {
+		position: fixed;
+		width: 100%;
+		top: 0px;
+		left: 0px;
+		background-color: black;
+		padding-top: 10px;
+		padding-bottom: 10px;
+	}
+	a {
+		font-size: 1.5rem;
+		font-family: Arial, Helvetica, sans-serif;
+		margin: auto 1rem;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Navbar from '../components/Navbar.svelte';
 	const copyright = new Date().getFullYear();
 </script>
 
+<Navbar />
 <div class="page-container">
 	<h1>Jonathan Dorety</h1>
 	<slot />
@@ -12,13 +14,13 @@
 
 <style>
 	h1 {
-		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',
-			Geneva, Verdana, sans-serif;
+		font-family: Arial, Helvetica, sans-serif;
 		font-size: 2.75rem;
 		text-align: center;
 	}
 	div.page-container {
 		max-width: 1200px;
 		margin: 0 auto;
+		margin-top: 60px;
 	}
 </style>
